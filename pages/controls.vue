148c788efa3fa335a1750e31425cc2572cfe1a78
Jon Jon <neumerance@live.com>
2021-11-02 08:00:33 +0800

    Ask user to landscape devise when in pportrait on mobile phones

--- pages/controls.vue
+++ pages/controls.vue
@@ -1,14 +1,43 @@
 <template>
   <div class="px-8 py-6">
-    <app-translation-control-buttons></app-translation-control-buttons>
-    <app-books-control-buttons></app-books-control-buttons>
-    <app-chapters-control-buttons></app-chapters-control-buttons>
-    <app-verses-control-buttons></app-verses-control-buttons>
+    <div v-if="isOnPortraitMode" class="h-screen flex flex-col center">
+      <h2 class="text-center">Please landscape your devise orientation</h2>
+    </div>
+    <div v-else>
+      <app-translation-control-buttons></app-translation-control-buttons>
+      <app-books-control-buttons></app-books-control-buttons>
+      <app-chapters-control-buttons></app-chapters-control-buttons>
+      <app-verses-control-buttons></app-verses-control-buttons>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: 'Controls',
-  middleware: 'require_session_token'
+  middleware: 'require_session_token',
+  data() {
+    return {
+      orientation: 0
+    }
+  },
+  computed: {
+    isOnPortraitMode() {
+      if (typeof screen.orientation !== 'undefined') {
+        return false;
+      }
+
+      return this.orientation == 0;
+    }
+  },
+  mounted() {
+    window.addEventListener("orientationchange", () => {
+      this.orientation = window.orientation;
+    }, false);
+  },
 }
 </script>
+<style scoped>
+  .center {
+    justify-content: center;
+  }
+</style>
--- pages/read.vue
+++ pages/read.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="read h-screen w-screen">
-    <div v-if="data && !data.not_found" class="w-full h-full px-8 py-6">
-      <app-bible-verse
-        :title="data.title"
-        :scripture="data.content"
-        :translation-code="data.translation_code"
-      >
-      </app-bible-verse>
+  <div>
+    <div v-if="isOnPortraitMode" class="h-screen flex flex-col center">
+      <h2 class="text-center">Please landscape your devise orientation</h2>
     </div>
-    <div v-else class="w-full h-full px-8 py-6">
-      <app-bible-verse-not-found>
-      </app-bible-verse-not-found>
+    <div v-else class="read h-screen w-screen">
+      <div v-if="data && !data.not_found" class="w-full h-full px-8 py-6">
+        <app-bible-verse
+          :title="data.title"
+          :scripture="data.content"
+          :translation-code="data.translation_code"
+        >
+        </app-bible-verse>
+      </div>
+      <div v-else class="w-full h-full px-8 py-6">
+        <app-bible-verse-not-found>
+        </app-bible-verse-not-found>
+      </div>
     </div>
   </div>
 </template>
@@ -22,19 +27,31 @@ export default {
   middleware: 'require_session_token',
   data() {
     return {
-      data: null
+      data: null,
+      orientation: 0
     }
   },
   computed: {
     ...mapGetters({
       sessionToken: 'getSessionToken'
-    })
+    }),
+    isOnPortraitMode() {
+      if (typeof screen.orientation !== 'undefined') {
+        return false;
+      }
+
+      return this.orientation == 0;
+    }
   },
   mounted() {
     this.$cable.subscribe({
       channel: 'ApplicationCable::BibleSessionsChannel',
       session_token: this.sessionToken
     });
+
+    window.addEventListener("orientationchange", () => {
+      this.orientation = window.orientation;
+    }, false);
   },
   channels: {
     'ApplicationCable::BibleSessionsChannel': {
@@ -52,5 +69,8 @@ export default {
   .read {
     background: url('~/assets/images/lush-green.webp');
     background-size: cover;
+  }
+  .center {
+    justify-content: center;
   }
 </style>
